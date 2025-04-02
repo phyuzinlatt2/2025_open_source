58349961d3fe394ec41cfc26a33d25de68767335
phyuzinlatt2 <phyuzinlatt1@gmail.com>
2025-04-02 16:22:49 +0900

    v0.3 kiosk.py

--- kiosk.py
+++ kiosk.py
@@ -1,7 +1,18 @@
-coffee_price = 2400
-cups = int(input("몇 잔 주문 하시나요? "))
-#print(cups * coffee_price)
-print(f"커피 {cups}잔을 주문하셨습니다\n총 금액은 {cups*coffee_price}원 입니다")
+# 리스트 -> 메뉴 리스트
 
+menus = [["아이스 아메리카노", 0], ["카페 라떼", 0], ["유자차", 0]]  # [[메뉴, 수량], ...]
+#prices = [2000, 2500, 2400]
 
+amount = int(input("몇 잔? "))
+
+for _ in range(amount):
+    menu = input(f"1) {menus[0][0]}   2) {menus[1][0]}   3) {menus[2][0]} : ")
+    if menu == "1":
+        menus[0][1] = menus[0][1] + 1
+    elif menu == "2":
+        menus[1][1] = menus[1][1] + 1
+    else:
+        menus[2][1] = menus[2][1] + 1
+
+print(f"총 금액은 {(menus[0][1] * 2000) + (menus[1][1] * 2500) + (menus[2][1] * 2400)}원 입니다.")
 
